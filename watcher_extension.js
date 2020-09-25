c2773c05a067458bc20e9f0fb38ac4b79f739ac6
David <genglanghuang@gmail.com>
2020-09-25 22:53:44 +0100

    change to ES6 version

--- watcher_extension.js
+++ watcher_extension.js
@@ -5,24 +5,27 @@ const { rawListeners } = require('process')
 let watchDir = './watch'
 let processedDir = './done'
 
-function Watcher(watchDir, processedDir) {
-    this.watchDir = watchDir
-    this.processedDir = processedDir
-}
-util.inherits(Watcher, events.EventEmitter)
+class Watcher extends events.EventEmitter {
+    constructor(watchDir, processedDir) {
+        super()
+        this.watchDir = watchDir
+        this.processedDir = processedDir
+    }
 
-Watcher.prototype.watch = function() {
-    fs.readdir(this.watchDir, (err, files) => {
-        if (err) throw err
-        for (var index in files) {
-            this.emit('process', files[index])
-        }
-    })
-}
-Watcher.prototype.start = function() {
-    fs.watchFile(this.watchDir, () => {
-        this.watch()
-    })
+    watch() {
+        fs.readdir(this.watchDir, (err, files) => {
+            if (err) throw err
+            for (var index in files) {
+                this.emit('process', files[index])
+            }
+        })
+    }
+
+    start() {
+        fs.watchFile(this.watchDir, () => {
+            this.watch()
+        })
+    }
 }
 
 let watcher = new Watcher(watchDir, processedDir)
@@ -30,7 +33,7 @@ let watcher = new Watcher(watchDir, processedDir)
 watcher.on('process', function (file) {
     let watchFile = this.watchDir + '/' + file
     let processedFile = this.processedDir + '/' + file.toLowerCase()
-    fs.rename(watchFile, processedFile, (err)=>{
+    fs.rename(watchFile, processedFile, (err) => {
         if (err) throw err
     })
 })
